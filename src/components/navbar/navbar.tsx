e63ba8f3cf3677db7c49b578c2240fec0a5eb3e2
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-17 22:58:09 +0300

    created navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import s from './navbar.module.sass'
+import { ReactComponent as Arrow } from '../../assets/images/Arrow.svg'
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className={s.navbar}>
+      <button className={s.topNavbar} onClick={() => setIsOpen(!isOpen)}>
+        <div className={s.block}>
+          <div className={s.textBlock}>
+            <span className={s.title}>Название проекта</span>
+            <span className={s.abbreviation}>Аббревиатура</span>
+          </div>
+          <Arrow className={`${ s.arrow } ${isOpen && `${s.activeArrow}`}`}/>
+        </div>
+      </button>
+      {isOpen &&
+        <div>
+
+        </div>
+      }
+    </div>
+  )
+}
