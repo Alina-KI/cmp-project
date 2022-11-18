0485cc8b4218de2518a1cf0a19d55bc174b5899d
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-18 21:33:43 +0300

    fixed header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import s from './header.module.sass'
+import {ReactComponent as Map} from '../../../assets/images/Map.svg'
+import {ReactComponent as ArrowBack} from '../../../assets/images/ArrowBack.svg'
 
 export const Header = () => {
   return (
-    <div>
-      jkl;
+    <div className={s.header}>
+      <div className={s.imageBlock}>
+        <button className={s.block}>
+          <Map className={s.image}/>
+        </button>
+        <button className={s.block}>
+          <ArrowBack className={s.image}/>
+        </button>
+      </div>
+     <div className={s.linkBlock}>
+       <a href='#' className={`${ s.link } ${s.linkActive}`}>Просмотр</a>
+       <a href='#' className={s.link}>Управление</a>
+       <div className={s.line}/>
+     </div>
     </div>
   )
 }
