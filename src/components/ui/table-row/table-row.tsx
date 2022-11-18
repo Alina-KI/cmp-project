ed0df9702eb4756f3940a5c6b4ddd6da94785499
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-18 08:12:19 +0300

    fixed table row

--- src/components/ui/table-row/table-row.tsx
+++ src/components/ui/table-row/table-row.tsx
@@ -1,26 +1,64 @@
-import React, { useState } from 'react'
+import React from 'react'
 import s from './table-row.module.sass'
-import { ListEntity } from '../../../types/listEntity'
+import { StoreListItem } from '../../../types/listItem'
 import { observer } from 'mobx-react-lite'
 import { ReactComponent as Image } from '../../../assets/images/Folder1.svg'
+import { entityStore } from '../../../store/entity-store'
 
 type Props = {
-  listItem: ListEntity
+  listItem: StoreListItem
 }
 
 export const TableRow = observer(({ listItem }: Props) => {
-  const [isEditMode, setIsEditMode] = useState(false)
 
   return (
-    <>
-      <div className={s.tableElement}>
-        <Image className={s.image}/>
-      </div>
-      <span className={s.tableElement}>{listItem.rowName}</span>
-      <span className={s.tableElement}>{listItem.salary}</span>
-      <span className={s.tableElement}>{listItem.equipmentCosts}</span>
-      <span className={s.tableElement}>{listItem.overheads}</span>
-      <span className={s.tableElement}>{listItem.estimatedProfit}</span>
-    </>
+    listItem.isEditMode
+      ?
+      <>
+        <div className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+          <Image className={s.image}/>
+        </div>
+        <input
+          className={`${s.tableElement} ${s.inputElement}`} onDoubleClick={() => entityStore.startEdit(listItem)}
+          placeholder={listItem.rowName}
+        />
+        <input
+          className={`${s.tableElement} ${s.inputElement}`} onDoubleClick={() => entityStore.startEdit(listItem)}
+          placeholder={listItem.salary.toString()}
+        />
+        <input
+          className={`${s.tableElement} ${s.inputElement}`} onDoubleClick={() => entityStore.startEdit(listItem)}
+          placeholder={listItem.equipmentCosts.toString()}
+        />
+        <input
+          className={`${s.tableElement} ${s.inputElement}`} onDoubleClick={() => entityStore.startEdit(listItem)}
+          placeholder={listItem.overheads.toString()}
+        />
+        <input
+          className={`${s.tableElement} ${s.inputElement}`} onDoubleClick={() => entityStore.startEdit(listItem)}
+          placeholder={listItem.estimatedProfit.toString().replace('.', ',')}
+        />
+      </>
+      :
+      <>
+        <div className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+          <Image className={s.image}/>
+        </div>
+        <span className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+        {listItem.rowName}
+      </span>
+        <span className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+        {listItem.salary}
+      </span>
+        <span className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+        {listItem.equipmentCosts}
+      </span>
+        <span className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+        {listItem.overheads}
+      </span>
+        <span className={s.tableElement} onDoubleClick={() => entityStore.startEdit(listItem)}>
+        {listItem.estimatedProfit.toString().replace('.', ',')}
+      </span>
+      </>
   )
 })
