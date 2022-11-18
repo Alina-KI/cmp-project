598e417ed30a1b795b1b1cbe69d672440c2020b2
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-18 22:08:47 +0300

    fixed header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import s from './header.module.sass'
-import {ReactComponent as Map} from '../../../assets/images/Map.svg'
-import {ReactComponent as ArrowBack} from '../../../assets/images/ArrowBack.svg'
+import { ReactComponent as Map } from '../../../assets/images/Map.svg'
+import { ReactComponent as ArrowBack } from '../../../assets/images/ArrowBack.svg'
 
 export const Header = () => {
   return (
     <div className={s.header}>
-      <div className={s.imageBlock}>
+      <div className={s.iconBlock}>
         <button className={s.block}>
-          <Map className={s.image}/>
+          <Map className={s.icon}/>
         </button>
         <button className={s.block}>
-          <ArrowBack className={s.image}/>
+          <ArrowBack className={s.icon}/>
         </button>
       </div>
-     <div className={s.linkBlock}>
-       <a href='#' className={`${ s.link } ${s.linkActive}`}>Просмотр</a>
-       <a href='#' className={s.link}>Управление</a>
-       <div className={s.line}/>
-     </div>
+      <div className={s.linkBlock}>
+        <a href="#" className={`${s.link} ${s.linkActive}`}>Просмотр</a>
+        <a href="#" className={s.link}>Управление</a>
+        <div className={s.line}/>
+      </div>
     </div>
   )
 }
