9dab0bf3281b2af4020f4c98a6764d4e9e84d379
Alina-KI <kraynova.alina.2002@mail.ru>
2022-11-19 16:11:31 +0300

    fixed get-nested-items-count

--- src/function/get-nested-items-count.ts
+++ src/function/get-nested-items-count.ts
@@ -1,5 +1,10 @@
 import { StoreListItem } from '../types/listItem'
 
 export const getNestedItemsCount = (list: StoreListItem) => {
-  return list.child.reduce((acc, item) => acc + 1 + item.child.length, 0)
+  return list.child.reduce((acc, item, idItem) => {
+    if (idItem === list.child.length - 1){
+      return acc + 1
+    }
+    return acc + 1 + item.child.length
+  }, 0)
 }
